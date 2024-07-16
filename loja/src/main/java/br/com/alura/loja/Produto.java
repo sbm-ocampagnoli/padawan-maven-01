d21cac014c3b5bd1f01341dd2de7411be09c0640
sbm-ocampagnoli <otavio.campagnoli@sbmtechnology.com.br>
2024-07-16 13:42:25 -0300

    Plugins

--- loja/src/main/java/br/com/alura/loja/Produto.java
+++ loja/src/main/java/br/com/alura/loja/Produto.java
@@ -1,5 +1,22 @@
 package br.com.alura.loja;
 
+import java.math.BigDecimal;
+
 public class Produto {
 
+	private String nome;
+	private BigDecimal preco;
+
+	public Produto(String nome, BigDecimal preco) {
+		this.nome = nome;
+		this.preco = preco;
+	}
+
+	public BigDecimal getPreco() {
+		return this.preco;
+	}
+
+	public String getNome() {
+		return this.nome;
+	}
 }
